8856fc5ee6a4f7e33749200781d146b9a6610fc3
abhishekshukla <abhishekshukla2022jan@cedcoss.com>
2022-04-18 09:43:36 +0530

    first part

---
+++ src/public/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    // $("#search").change(function (event) {
+    //     alert('ay');
+
+    // });
+    $("#search").keyup(function(){
+        // $("#search").css("background-color", "pink");
+        // let sel1 = $("#search").val();
+        // console.log($("#search").val());
+      });
+
+});
